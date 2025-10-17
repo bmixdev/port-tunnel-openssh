32eb3b96f3ce232093995508e42b5f13b9f7a64f
Belin Mikhail <codxxx@gmail.com>
2025-10-17 13:04:20 +0300

    fix build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,15 @@
 $ErrorActionPreference = "Stop"
-Write-Host "Compiling..."
-javac src/main/java/PortTunnelOpenSSH.java
-Write-Host "Packaging JAR..."
-jar --create --file port-tunnel-oss-win.jar --main-class=PortTunnelOpenSSH -C src/main/java PortTunnelOpenSSH.class
-Write-Host "Done: port-tunnel-oss-win.jar"
+
+if (Test-Path out) { Remove-Item out -Recurse -Force }
+javac -d out src/main/java/PortTunnelOpenSSH.java
+
+@"
+Manifest-Version: 1.0
+Main-Class: PortTunnelOpenSSH
+
+"@ | Out-File -Encoding ascii manifest.mf
+
+# Кладём ВСЁ из ./out → исключаем проблемы с '$'
+jar cfm port-tunnel-oss-win.jar manifest.mf -C out .
+
+Write-Host "Built: port-tunnel-oss-win.jar"
